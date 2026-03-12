ca7fa40dd2062e31331b64cdd7e7ac0931848dc0
Jackson Steinkamp <jackson.steinkamp@crpusd.org>
2026-03-11 18:05:35 -0700

    added mouse (claude carried icl)

--- zoom.py
+++ zoom.py
@@ -1,11 +1,8 @@
+#CHAT STARTS
 import ctypes
 from ctypes import wintypes
 import time
-import keyboard, mouse 
-
-#------------------------------------------------------------------------------------------------------
-#CHATGPT START (Though TBH I probably won't turn this file in at all)
-#------------------------------------------------------------------------------------------------------
+import keyboard
 
 magnification = ctypes.WinDLL("Magnification.dll")
 user32 = ctypes.windll.user32
@@ -18,7 +15,7 @@ SCREEN_WIDTH = user32.GetSystemMetrics(0)
 SCREEN_HEIGHT = user32.GetSystemMetrics(1)
 print("Finding Monitor Specs...")
 print("Screen Width: " + str(SCREEN_WIDTH) + "\nScreen Height: " + str(SCREEN_HEIGHT))
-time.sleep(1.5)
+time.sleep(1.5) #TODO expand on this
 
 def set_centered_zoom(zoom_level: float):
     visible_width = SCREEN_WIDTH / zoom_level
@@ -26,10 +23,22 @@ def set_centered_zoom(zoom_level: float):
     offset_x = int((SCREEN_WIDTH - visible_width) / 2) 
     offset_y = int((SCREEN_HEIGHT - visible_height) / 2)
     magnification.MagSetFullscreenTransform(zoom_level, offset_x, offset_y)
-    
-#------------------------------------------------------------------------------------------------------
-#CHATGPT ENDS, MY CODE BEGINS
-#------------------------------------------------------------------------------------------------------
+
+VK_MOUSE = {
+    "left": 0x01,
+    "right": 0x02,
+    "middle": 0x04,
+    "x": 0x05,
+    "x2": 0x06,
+}
+
+def is_key_pressed(key):
+    if key in VK_MOUSE:
+        return bool(user32.GetAsyncKeyState(VK_MOUSE[key]) & 0x8000)
+    else:
+        return keyboard.is_pressed(key)
+
+#CHAT ENDS
 
 def clear():
     print("\033[2J\033[H", end="") 
@@ -39,12 +48,12 @@ def main(data):
     zoom_level = 1
     clear()
     print("Magnifier at 1x")
-    name, numkeys, keys = data
+    keys = data[2]
     quit = 0
     while True:
         pressed = False
         for mode, key, zooms in keys:
-            if keyboard.is_pressed(key):
+            if is_key_pressed(key):
                 if mode == "Hold":
                     if zoom_level != zooms[0]:
                         zoom_level = zooms[0]
@@ -53,7 +62,7 @@ def main(data):
                         print("Magnifier at " + str(zoom_level) + "x")
                 elif mode == "Toggle" or mode == "AWP":
                     clear()
-                    print("Mode not available (Coming soon!)") #TODO Fix
+                    print("Mode not available (Coming soon!)")
                 pressed = True
                 break
         if not pressed and zoom_level != 1:
@@ -77,3 +86,7 @@ def main(data):
         else:
             quit = 0
         time.sleep(0.05)
+
+
+import presetanalyzer
+main(presetanalyzer.readpresetdata("Test | 2 (Hold : p : 1.25)(Hold : left : 1.25) <>"))
